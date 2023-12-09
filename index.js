c7084bf1d22f9a6415eb3ae06faa39458a5bf0e4
acudoc <cdobratz@protonmail.com>
2023-12-08 21:52:40 -0700

    Add Parser and WhoAmI functions

--- index.js
+++ index.js
@@ -5,11 +5,15 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+var requestIp = require('request-ip');
+var accepts = require('accepts');
+var UAParser = require('ua-parser-js');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(requestIp.mw());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -23,6 +27,24 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/whoami', function (req, res) {
+  var ipaddress = req.clientIp;
+  var language = accepts(req).languages()[0] || 'en-US'; // Default to 'en-US' if language is not provided
+
+  // Parse user agent string using ua-parser-js
+  var userAgent = req.headers['user-agent'];
+  var parser = new UAParser();
+  var result = parser.setUA(userAgent).getResult();
+  var software = result.browser.name + ' ' + result.browser.version;
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
+});
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
